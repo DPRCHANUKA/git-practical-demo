c7058b49c6b1fc04b7c68f5f761fdb5c252a153b
DPRCHANUKA <rukshanchanuka.2002@gmail.com>
2026-03-14 10:55:58 +0530

    version 2:Added multiple function

--- numbers.py
+++ numbers.py
@@ -1,9 +1,13 @@
-# numbers.py - Version 1
+# numbers.py - Version 2
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(5, 3))
+print("Multiplication:", multiply(5, 3))
