4298d5ec3e455c579978dadb97b57c434be00981
DPRCHANUKA <rukshanchanuka.2002@gmail.com>
2026-03-14 10:57:59 +0530

    version 3:added divide function (has bug)

--- numbers.py
+++ numbers.py
@@ -1,4 +1,4 @@
-# numbers.py - Version 2
+# numbers.py - Version 3 (with bug)
 def add(a, b):
     return a + b
 
@@ -8,6 +8,10 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    return a / b  # This will crash if b = 0!
+
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(5, 3))
 print("Multiplication:", multiply(5, 3))
+print("Division:", divide(5, 0))  # BUG: Division by zero!
